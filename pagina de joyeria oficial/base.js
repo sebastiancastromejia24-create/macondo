b5768aea8901e1b6c9966828dd6fe71a8323ad22
millosviasus-design <millosviasus@gmail.com>
2026-05-27 17:19:33 -0500

    fix clevercloud deploy

---
+++ pagina de joyeria oficial/base.js
@@ -0,0 +1,74 @@
+async function login(event) {
+
+    event.preventDefault();
+
+    const correo = document.getElementById("correo").value;
+
+    const password = document.getElementById("password").value;
+
+    try {
+
+        const response = await fetch(`${API_URL}/api/auth/login`, {
+
+            method: "POST",
+
+            headers: {
+                "Content-Type": "application/json"
+            },
+
+            body: JSON.stringify({
+
+                correoElectronico: correo,
+
+                password: password
+
+            })
+
+        });
+
+        if (response.ok) {
+
+            const data = await response.json();
+
+            localStorage.setItem("token", data.token);
+
+            alert("Inicio de sesión exitoso");
+
+            window.location.href = "index.html";
+
+        } else {
+
+            alert("Correo o contraseña incorrectos");
+
+        }
+
+    } catch (error) {
+
+        console.error(error);
+
+        alert("Error al conectar con el servidor");
+
+    }
+
+}
+
+     let slides = document.querySelectorAll('.slide');
+
+            let index = 0;
+
+            function changeSlide(){
+
+                slides[index].classList.remove('active');
+
+                index++;
+
+                if(index >= slides.length){
+                    index = 0;
+                }
+
+                slides[index].classList.add('active');
+            }
+
+            /* CAMBIO AUTOMÁTICO */
+
+            setInterval(changeSlide, 4000);
